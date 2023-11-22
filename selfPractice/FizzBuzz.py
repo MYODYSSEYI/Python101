13e425eb6486429fed2d19661cba501e876ad8ee
MYODYSSEYI <luca.gatty@outlook.com>
2023-11-22 14:58:39 +0100

    FizzBuzz

---
+++ selfPractice/FizzBuzz.py
@@ -0,0 +1,18 @@
+
+'''
+When this code is finished...
+it should start in the Terminal, count from 0 to 100 and put out 
+Fizz if something is only dividable by 3 
+Buzz if something is only dividable by 5
+FizzBuzz if something is dividable by both.
+'''
+i = 0
+for i in range(101):
+    print(i)
+
+if ((i%5==0)&(i%3==0)):
+    print("FizzBuzz")
+elif (i%3==0):
+    print("Fizz")
+elif (i%5==0):
+    print("Buzz")

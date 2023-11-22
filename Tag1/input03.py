57c8470cbe25a7587dae795d35b6fd20f2c08715
MYODYSSEYI <luca.gatty@outlook.com>
2023-11-22 17:05:00 +0100

    COMMIT

--- Tag1/input03.py
+++ Tag1/input03.py
@@ -1,16 +1,12 @@
 while True:
     print("Wähle zwischen \n [+] Addition \n [-] Subtraktion \n [*] Multiplikation \n [/] Division \n [**] Potenz \n [%] Modulo \n [x] exit")
-
     var_Rechnungsart = input("Ihre Auswahl: ")
-
     if var_Rechnungsart == "x":
         print("Bye!")
         break
-
     elif var_Rechnungsart not in ["+", "-", "*", "/", "**", "%"]:
         print("Try again!")
         continue
-
     try:
         var_a = float(input("a = "))
         var_b = float(input("b = "))
@@ -18,27 +14,18 @@ while True:
     except ValueError:
         print("Only numbers BOI! Try again.")
         continue
-
     # if statements
     if var_Rechnungsart == "+":
         var_ergebnis = var_a + var_b + var_c
-
     elif var_Rechnungsart == "-":
         var_ergebnis = var_a - var_b - var_c
-
     elif var_Rechnungsart == "*":
         var_ergebnis = var_a * var_b * var_c
-
     elif var_Rechnungsart == "/":
         var_ergebnis = var_a / var_b / var_c
-
     elif var_Rechnungsart == "**":
         var_ergebnis = var_a ** var_b
-
     elif var_Rechnungsart == "%":
-
         var_ergebnis = var_a % var_b
-
         print(var_ergebnis)
         continue
-        

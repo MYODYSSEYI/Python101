ee3f62afa4027e1f272d6bb11a4f8371b5466fa8
MYODYSSEYI <luca.gatty@outlook.com>
2023-11-24 22:49:42 +0100

    COMMIT

---
+++ selfPractice/codeCamp.py
@@ -0,0 +1,105 @@
+'''
+from math import*
+
+
+#strings
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+phrase = "Pythong hehe"
+print(phrase.replace("y", "c"))
+
+
+#numbers 
+
+myNum = -5
+print(str(abs(myNum)) + ' my fav number')
+print(pow(3,100))
+print(max(4,9))
+print(min(100, 9))
+print(round(7.9))
+print(floor(3.7))
+print(ceil(7.3))
+print(sqrt(99))
+
+
+#user input
+
+name = input('Enter yout name: ')
+age = input('Enter your age: ')
+print('Hello ' + name + '!\n' + 'You are ' + age + '!')
+'''
+'''
+#Simple Calculator
+
+num1 = input('Enter a number: ')
+num2 = input('Enter a number: ')
+
+result = float(num1+num2)
+
+print(result)
+'''
+
+'''
+#mad lips game 
+color = input("enter a color: ")
+plural_noun = input("enter a plural noun: ")
+celeb = input("name of a celebrity: ")
+print(f"Roses are {color}")
+print(f"{plural_noun} are blue")
+print(f"I love {celeb}")
+'''
+
+'''
+#lists
+
+friends = ['Hanna', 'Nikita', 'Niki']
+
+girlfriend = print(friends[0])
+friends = print(friends[1:])
+'''
+
+
+#lists 2 
+
+#luckyNumbers = [2, 7, 42, 666, 1000]
+#animals = ['Tiger', 'Lion', 'Dolphin', 'Cheetar', 'Peguin']
+
+#extends arrays with varialble or whatever u want
+#luckyNumbers.extend(animals)
+
+#adds whatever u want to the end of the array
+#luckyNumbers.append("hello")
+
+#inserts item to secified spot in array
+#luckyNumbers.insert(1, "ugga")
+
+#removes specified item from array
+#luckyNumbers.remove(1000)
+
+#clears array
+#luckyNumbers.clear()
+
+#pop removes last item from array
+#luckyNumbers.pop()
+
+#shows if item is in array and on what spot
+#print(luckyNumbers.index(666))
+
+#shows how often a value is in an array
+#print(luckyNumbers.count(666))
+
+#sorts array in order 
+#luckyNumbers.sort()
+
+#reverses array
+#luckyNumbers.reverse()
+
+#copys arrays
+#luckyNumbers2 = luckyNumbers.copy()
+
+#print(luckyNumbers)
+
+

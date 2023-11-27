2af859e0e3b9aec567268fcd70968a4da672c45a
MYODYSSEYI <luca.gatty@outlook.com>
2023-11-27 10:46:03 +0100

    timer

---
+++ Tag2/Timer.py
@@ -0,0 +1,28 @@
+import time
+import os
+
+varStop = "n"
+varSeconds = int(input("Seconds: "))
+varMinutes = int(input("Minutes: "))
+varHours = int(input("Hours: "))
+
+
+while varStop <= "n":
+    os.system("cls" if os.name == "nt" else clear)
+    print(f"{varHours}:{varMinutes}:{varSeconds}")
+    print("\n")
+    varSeconds -= 1
+    time.sleep(1)
+    if varSeconds == -1: 
+        varSeconds = 59 
+        varMinutes -= 1
+    if varMinutes == -1:
+        varMinutes = 59
+        varHours -= 1
+    if varHours ==0 and varMinutes == 0 and varSeconds == 0:
+        time.sleep(1) 
+        varStop = "stop"
+        os.system("cls" if os.name == "nt" else clear)
+        print(f"{0}:{0}:{0} \nFINISHED!")
+        exit()
+

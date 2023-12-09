8106f551d126ee53e2313a5486c3c159da806288
MYODYSSEYI <luca.gatty@outlook.com>
2023-12-09 10:39:30 +0100

    the beginning of a journey

---
+++ selfPractice/PYCRAWL.py
@@ -0,0 +1,28 @@
+print('''
+
+ ██▓███ ▓██   ██▓ ▄████▄   ██▀███   ▄▄▄       █     █░ ██▓
+▓██░  ██▒▒██  ██▒▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▓█░ █ ░█░▓██▒
+▓██░ ██▓▒ ▒██ ██░▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ▒█░ █ ░█ ▒██░
+▒██▄█▓▒ ▒ ░ ▐██▓░▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██ ░█░ █ ░█ ▒██░
+▒██▒ ░  ░ ░ ██▒▓░▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒░░██▒██▓ ░██████▒
+▒▓▒░ ░  ░  ██▒▒▒ ░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▓░▒ ▒  ░ ▒░▓  ░
+░▒ ░     ▓██ ░▒░   ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░  ▒ ░ ░  ░ ░ ▒  ░
+░░       ▒ ▒ ░░  ░          ░░   ░   ░   ▒     ░   ░    ░ ░
+         ░ ░     ░ ░         ░           ░  ░    ░        ░  ░
+         ░ ░     ░
+
+''')
+
+import time
+
+Name = input("Please enter your name if you wish to start this Adventure.\n> ")
+
+print() # Move to next line
+
+text = f"This is the story of a Warrior named {Name} many ."
+
+for char in text:
+    print(char, end='', flush=True)
+    time.sleep(0.1)  # Adjust the sleep duration (in seconds) for your preferred speed
+
+print()  # Move to the next line after printing

5c45d14bd1a8b7411c451d0309d333d024ae2e98
MYODYSSEYI <luca.gatty@outlook.com>
2024-01-29 13:09:04 +0100

    working ''

--- gessel/numberGuesser.py
+++ gessel/numberGuesser.py
@@ -1,32 +1,38 @@
 import random 
 
-print('''
- ███╗   ██╗ ██╗   ██╗ ███╗   ███╗ ██████╗  ███████╗ ██████╗       ██████╗  ██╗   ██╗ ███████╗ ███████╗ ███████╗ ███████╗ ██████╗ 
- ████╗  ██║ ██║   ██║ ████╗ ████║ ██╔══██╗ ██╔════╝ ██╔══██╗     ██╔════╝  ██║   ██║ ██╔════╝ ██╔════╝ ██╔════╝ ██╔════╝ ██╔══██╗
- ██╔██╗ ██║ ██║   ██║ ██╔████╔██║ ██████╔╝ █████╗   ██████╔╝     ██║  ███╗ ██║   ██║ █████╗   ███████╗ ███████╗ █████╗   ██████╔╝
- ██║╚██╗██║ ██║   ██║ ██║╚██╔╝██║ ██╔══██╗ ██╔══╝   ██╔══██╗     ██║   ██║ ██║   ██║ ██╔══╝   ╚════██║ ╚════██║ ██╔══╝   ██╔══██╗
- ██║ ╚████║ ╚██████╔╝ ██║ ╚═╝ ██║ ██████╔╝ ███████╗ ██║  ██║     ╚██████╔╝ ╚██████╔╝ ███████╗ ███████║ ███████║ ███████╗ ██║  ██║
- ╚═╝  ╚═══╝  ╚═════╝  ╚═╝     ╚═╝ ╚═════╝  ╚══════╝ ╚═╝  ╚═╝      ╚═════╝   ╚═════╝  ╚══════╝ ╚══════╝ ╚══════╝ ╚══════╝ ╚═╝  ╚═╝
+while True:
+    print('''
+     ███╗   ██╗ ██╗   ██╗ ███╗   ███╗ ██████╗  ███████╗ ██████╗       ██████╗  ██╗   ██╗ ███████╗ ███████╗ ███████╗ ███████╗ ██████╗ 
+     ████╗  ██║ ██║   ██║ ████╗ ████║ ██╔══██╗ ██╔════╝ ██╔══██╗     ██╔════╝  ██║   ██║ ██╔════╝ ██╔════╝ ██╔════╝ ██╔════╝ ██╔══██╗
+     ██╔██╗ ██║ ██║   ██║ ██╔████╔██║ ██████╔╝ █████╗   ██████╔╝     ██║  ███╗ ██║   ██║ █████╗   ███████╗ ███████╗ █████╗   ██████╔╝
+     ██║╚██╗██║ ██║   ██║ ██║╚██╔╝██║ ██╔══██╗ ██╔══╝   ██╔══██╗     ██║   ██║ ██║   ██║ ██╔══╝   ╚════██║ ╚════██║ ██╔══╝   ██╔══██╗
+     ██║ ╚████║ ╚██████╔╝ ██║ ╚═╝ ██║ ██████╔╝ ███████╗ ██║  ██║     ╚██████╔╝ ╚██████╔╝ ███████╗ ███████║ ███████║ ███████╗ ██║  ██║
+     ╚═╝  ╚═══╝  ╚═════╝  ╚═╝     ╚═╝ ╚═════╝  ╚══════╝ ╚═╝  ╚═╝      ╚═════╝   ╚═════╝  ╚══════╝ ╚══════╝ ╚══════╝ ╚══════╝ ╚═╝  ╚═╝
 
-'''
-)
+    '''
+    )
 
 # pre game config
-start = int(input("Pick a number to start from: "))
-to = int(input("Pick a number to end at: "))
-tries = 1
-number = random.randint(start, to)
+    start = int(input("Pick a number to start from: "))
+    to = int(input("Pick a number to end at: "))
+    tries = 1
+    number = random.randint(start, to)
 
 # game
-guess = int(input(f"Pick a number between {start} and {to}: "))
-while guess != number:
-    tries += 1
-    if guess < start or guess > to:
-        print(f"I said between {start} and {to}!")
-    elif guess < number:
-        print("Too low")
-    elif guess > number:
-        print("Too high")
-    guess = int(input("Try again: "))
-    
-print(f"\nYou got it! \nYou needed {tries} tries and the number was {number}.")
+    guess = int(input(f"Pick a number between {start} and {to}: "))
+    while guess != number:
+        tries += 1
+        if guess < start or guess > to:
+            print(f"I said between {start} and {to}!")
+        elif guess < number:
+            print("Too low")
+        elif guess > number:
+            print("Too high")
+        guess = int(input("Try again: "))
+        
+    print(f"\nYou got it! \nYou needed {tries} tries and the number was {number}.")
+
+    again = input("Would you like to play again? (y/n): ")
+
+    if again.lower() != "y":
+        break
